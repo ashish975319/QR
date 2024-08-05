bccdd300d6b8387f4b3ad24255f44128ccaf5213
Ashish Dahiya <124877024+ashish975319@users.noreply.github.com>
2024-08-05 18:41:02 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import QRCodeForm from "./components/QRCodeForm";
-import ScannedDataDisplay from "./components/ScannedDataDisplay";
+import ScannedDataDisplay from "./components/ScannedDataPage";
 
 const App = () => {
   return (
